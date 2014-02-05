a3c762746aa6c566ff88b3ff9f0ed9527f476d2b
yupenglu <lyp806@gmail.com>
2014-02-05 16:41:31 -0500

    add javadoc to the file

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -4,11 +4,22 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * @author Yupeng Lu
+ *
+ * @param <V> type of the object of the tree's node value
+ */
 public class Tree<V> {
     
     private V value;
     private ArrayList<Tree<V>> child = new ArrayList<Tree<V>>();
     
+    /**
+     * Constructs object Tree, by either giving the root value,
+     * or giving the root value and its children
+     * @param value The value of root
+     * @param children Children Trees that is adding to the root
+     */
     @SafeVarargs
     public Tree(V value, Tree<V>... children) {
         this.value = value;
@@ -17,36 +28,69 @@ public class Tree<V> {
         }
     }
     
+    /**
+     * @return The value of the node.
+     */
     public V getValue() {
+    	if (this.value == null) return null;
         return value;
     }
     
+    /**
+     * @return The first child of this tree.
+     */
     public Tree<V> firstChild() {
+    	if (this.isLeaf()) return null;
         return child.get(0);
     }
     
+    /**
+     * @return The last child of this tree (or null if this tree is a leaf).
+     */
     public Tree<V> lastChild() {
+    	if (this.isLeaf()) return null;
         return child.get(child.size() - 1);
     }
     
+    /**
+     * @return The number of (immediate) children of this node.
+     */
     public int numberOfChildren() {
         return child.size();
     }
     
+    /**
+     * @param index the index of the child trying to get
+     * @return The index-th child of this tree (counting from zero, as with arrays)
+     * @throws NoSuchElementException if index is less than zero or greater than 
+     * or equal to the number of children.
+     */
     public Tree<V> child(int index) throws NoSuchElementException {
         if (index < 0 || index >= child.size())
             throw new NoSuchElementException();
         return child.get(index);
     }
     
+    /**
+     * @return An iterator for the children of this tree. 
+     */
     public Iterator<Tree<V>> children() {
         return child.iterator();
     }
     
+    /**
+     * @return False if this node has children.
+     */
     public boolean isLeaf() {
         return this.numberOfChildren() == 0;
     }
     
+    /**
+     * Tests whether this tree contains the given tree(node).
+     * The root of this tree is included in the recursive search.
+     * @param node The Tree is going to test.
+     * @return True if this tree contains the given node
+     */
     private boolean contains(Tree<V> node) {
         Iterator<Tree<V>> childIterator = children();
         if (this == node) return true;
@@ -57,18 +101,32 @@ public class Tree<V> {
         return false;
     }
     
+    /**
+     * Compares this tree with given tree to see whether they are equal
+     * @return True if (1) the given object is a Tree,
+     * and (2) the value fields of the two trees are equal,
+     * and (3) each child of one Tree equals the corresponding child of the other Tree.
+     */
     @Override
     public boolean equals(Object object) {
         if (!(object instanceof Tree<?>)) {
             return false;
         } else {
             if (((Tree<?>) object).isLeaf()) {
-                return (((Tree<?>) object).value == value);
+            	if (((Tree<?>) object).getValue() == null || this.value == null)
+            		return this.value == null;
+            	else
+            		return (((Tree<?>) object).getValue().equals(this.value));
             } else {
                 // if two nodes have different value, then return false
-                if (! value.equals(((Tree<?>) object).getValue())) return false;
+            	if (((Tree<?>) object).getValue() == null || this.value == null)
+            		return this.value == null;
+            	else
+            		if (! value.equals(((Tree<?>) object).getValue())) return false;
+            	
                 if (! (this.numberOfChildren() == ((Tree<?>) object).numberOfChildren()))
                     return false;
+                
                 for (int i = 0; i < numberOfChildren(); i++) {
                     if (!this.child(i).equals(((Tree<?>) object).child(i))) return false;
                 }
@@ -77,43 +135,74 @@ public class Tree<V> {
         }
     }
     
+    /**
+     * @return A multiline representation of this Tree.
+     * Each line contains the toString() representation of the value in that node,
+     * terminated with a newline (\n). Each child is indented two spaces under its parent.
+     */
     @Override
     public String toString() {
-//        if (getValue() == null)           
-//            return "";
-//        } else {
-//            System.out.println("is null");
         return toStringHelper(1);
     }
     
+    /**
+     * toString helper method
+     * @param depth Used to decide how many spaces in front
+     * @return Formatted String
+     */
     private String toStringHelper(int depth) {
-        String result;
-        result = getValue().toString();
+        String result = "";
+        if (this.getValue() != null) {
+        	result = getValue().toString();
+        }
         result += "\n";
         for (int i = 0; i < this.numberOfChildren(); i++) {
-            for (int j = 0; j < depth; j++) {
-                result += "  ";
-            }
+        	for (int j = 0; j < depth; j++) {
+        		result += "  ";
+        	}
             result += this.child(i).toStringHelper(depth + 1);
         }
         return result;
     }
     
+    /**
+     * Sets the value in this node of the Tree.
+     * @param value An object of Class V
+     */
     public void setValue(V value) {
         this.value = value;
     }
     
+    /**
+     * Adds newChild as the new last child of this tree,
+     * provided that the resultant tree is valid.
+     * @param newChild the new tree that is going to add
+     * @throws IllegalArgumentException when the resultant tree is not valid
+     */
     public void addChild(Tree<V> newChild) throws IllegalArgumentException {
         if (newChild.contains(this)) throw new IllegalArgumentException();
         child.add(newChild);
     }
     
+    /**
+     * Adds newChild as the new index-th child of this tree (counting from zero),
+     * provided that the resultant tree is valid. 
+     * @param index the index of the newChild
+     * @param newChild the tree that is going to add
+     * @throws IllegalArgumentException when the resultant tree is not valid
+     */
     public void addChild(int index, Tree<V> newChild) throws IllegalArgumentException {
         if (newChild.contains(this)) throw new IllegalArgumentException();
         if (index < 0 || index >= this.numberOfChildren()) throw new IllegalArgumentException();
         child.add(index, newChild);
     }
     
+    /**
+     * Adds each Tree in children as a new child of this tree node,
+     * after any existing children, provided that the resultant tree is valid
+     * @param children An array of Tree<V> that is going to add
+     * @throws IllegalArgumentException when the resultant tree is not valid
+     */
     @SafeVarargs
     public final void addChildren(Tree<V>... children ) throws IllegalArgumentException {
         for (int i = 0; i < children.length; i++) {
@@ -126,7 +215,7 @@ public class Tree<V> {
      * Removes and returns the index-th child of this tree,
      * or throws a NoSuchElementException if the index is illegal. 
      * @param index the index of child want to remove
-     * @return the index-th child of the tree
+     * @return The index-th child of the tree
      * @throws NoSuchElementException when index is illegal
      */
     public Tree<V> removeChild(int index) throws NoSuchElementException {
--- src/tree/TreeTest.java
+++ src/tree/TreeTest.java
@@ -1,16 +1,24 @@
 package tree;
 
 import static org.junit.Assert.*;
-
+import java.util.NoSuchElementException;
 import java.util.Iterator;
 
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author Yupeng Lu
+ * JUnit Test Cases for Class Tree
+ */
 public class TreeTest {
 
-    Tree<String> tree;
+    private Tree<String> tree;
     
+    /**
+     * set up method, runs before each test case
+     * @throws Exception when trying to generating invalid tree
+     */
     @Before
     public void setUp() throws Exception {
         tree = new Tree<String>("3");
@@ -21,6 +29,9 @@ public class TreeTest {
         tree.addChildren(new Tree<String>("24"), new Tree<String>("35"));
     }
 
+    /**
+     * Test case of the constructor
+     */
     @Test
     public void testTree() {
         Tree<String> tree2 = new Tree<String>("1234", new Tree<String>("55555"));
@@ -28,31 +39,67 @@ public class TreeTest {
         assertEquals("3\n  11\n  42\n    110\n      1234\n        55555\n  24\n  35\n", tree.toString());
     }
 
+    /**
+     * Test case of the getValue() method
+     */
     @Test
     public void testGetValue() {
         assertEquals("3", tree.getValue());
+        
+        // test when the value is null
+        tree.child(0).setValue(null);
+        assertTrue(tree.child(0).getValue() == null);
     }
 
+    /**
+     * Test case of the firstChild() method
+     */
     @Test
     public void testFirstChild() {
         assertEquals(new Tree<String>("11"), tree.firstChild());
+        
+        // should return null when the node is a leaf
+        assertTrue(tree.child(0).firstChild() == null);
     }
 
+    /**
+     * Test case of the lastChild() method
+     */
     @Test
     public void testLastChild() {
         assertEquals(new Tree<String>("35"), tree.lastChild());
+        
+        // should return null when the node is a leaf
+        assertTrue(tree.child(0).lastChild() == null);
     }
 
+    /**
+     * Test case of the numberOfChildren() method
+     */
     @Test
     public void testNumberOfChildren() {
         assertEquals(4, tree.numberOfChildren());
     }
 
+    /**
+     * Test case of the child(int) method
+     */
     @Test
     public void testChild() {
         assertEquals(new Tree<String>("24"), tree.child(2));
     }
+    
+    /**
+     * Test case of the child(int) method throwing exceptions
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void testChildThrowException() {
+        tree.child(5);
+    }
 
+    /**
+     * Test case of the children() method
+     */
     @Test
     public void testChildren() {
         Iterator<Tree<String>> iterator = tree.children();
@@ -61,25 +108,51 @@ public class TreeTest {
             test += iterator.next().getValue() + " ";
         }
         assertEquals("11 42 24 35 ", test);
+        
+        // test empty iterator when node is a leaf
+        iterator = tree.child(0).children();
+        test = "";
+        while (iterator.hasNext()) {
+            test += iterator.next().getValue() + " ";
+        }
+        assertEquals("", test);
     }
 
+    /**
+     * Test case of the isLeaf() method
+     */
     @Test
     public void testIsLeaf() {
         assertTrue(tree.child(0).isLeaf());
         assertFalse(tree.child(1).isLeaf());
     }
 
+    /**
+     * Test case of the equals(Object) method
+     */
     @Test
     public void testEqualsObject() {
-        Tree<String> tree3 = new Tree<String>("42");
-        assertTrue(tree.child(1).equals(tree3));
+        Tree<String> tempTree = new Tree<String>("42");
+        assertTrue(tree.child(1).equals(tempTree));
+        assertFalse(tree.equals(tempTree));
+        assertTrue(tree.equals(tree));
+        assertFalse(tree.child(1).equals("42"));
+        
+        tree.setValue(null);
+        assertTrue(tree.equals(tree));
     }
 
+    /**
+     * Test case of the toString() method
+     */
     @Test
     public void testToString() {
         assertEquals("3\n  11\n  42\n    110\n  24\n  35\n", tree.toString());
     }
 
+    /**
+     * Test case of the setValue(V) method
+     */
     @Test
     public void testSetValue() {
         Tree<String> treeFour = new Tree<String>("3", tree.child(0), tree.child(1), tree.child(2), tree.child(3));
@@ -89,11 +162,17 @@ public class TreeTest {
         assertEquals("8\n  11\n  42\n    110\n  24\n  35\n", tree.toString());
     }
 
+    /**
+     * Test case of the addChild(Tree<V>) method throwing exception
+     */
     @Test(expected = IllegalArgumentException.class)
     public void testAddChildThrowException() {
         tree.addChild(tree);
     }
     
+    /**
+     * Test case of the addChild(Tree<V>) method
+     */
     @Test
     public void testAddChildTreeOfV() {
         Tree<String> tempTree = new Tree<String>("48");
@@ -101,6 +180,9 @@ public class TreeTest {
         assertEquals("3\n  11\n  42\n    110\n  24\n  35\n  48\n", tree.toString());
     }
 
+    /**
+     * Test case of the addChild(int, Tree<V>) method
+     */
     @Test
     public void testAddChildIntTreeOfV() {
         Tree<String> tempTree = new Tree<String>("48");
@@ -110,6 +192,9 @@ public class TreeTest {
         assertEquals("3\n  11\n  48\n  42\n    48\n    110\n  24\n  35\n", tree.toString());
     }
 
+    /**
+     * Test case of the addChildren(Tree<V>...) method
+     */
     @Test
     public void testAddChildren() {
         Tree<String> tempTreeOne = new Tree<String>("48");
@@ -119,15 +204,13 @@ public class TreeTest {
         assertEquals("3\n  11\n  42\n    110\n  24\n  35\n  48\n  58\n  68\n", tree.toString());
     }
 
+    /**
+     * Test case of the remove(int) method
+     */
     @Test
     public void testRemoveChild() {
         Tree<String> tempTree = tree.removeChild(1);
         assertEquals("3\n  11\n  24\n  35\n", tree.toString());
         assertEquals("42\n  110\n", tempTree.toString());
     }
-
-//    @Test
-//    public void testObject() {
-//        fail("Not yet implemented");
-//    }
 }
